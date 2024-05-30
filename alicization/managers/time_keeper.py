22bd4e86cb24bf1141b834015e3eb56454472803
Ben Lai <laichunpongben@gmail.com>
2024-05-30 08:59:05 +0800

    feat: add time keeper

---
+++ alicization/managers/time_keeper.py
@@ -0,0 +1,18 @@
+# time_keeper.py
+
+
+class TimeKeeper:
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(TimeKeeper, cls).__new__(cls)
+            cls.__instance._turn = 1
+        return cls.__instance
+
+    @property
+    def turn(self):
+        return self._turn
+
+    def tick(self):
+        self._turn += 1
