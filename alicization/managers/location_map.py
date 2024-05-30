23a510e29a4718a88074dfab4737326b0181fdcd
Ben Lai <laichunpongben@gmail.com>
2024-05-30 09:00:00 +0800

    feat: add location map

---
+++ alicization/managers/location_map.py
@@ -0,0 +1,28 @@
+# location_map.py
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LocationMap:
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(LocationMap, cls).__new__(cls)
+            cls.__instance._map = {}
+        return cls.__instance
+
+    @property
+    def map(self):
+        return self._map
+
+    def get_location(self, location_name):
+        return self._map.get(location_name)
+
+    def add_location(self, location):
+        if not location.name in self._map:
+            self._map[location.name] = location
+        else:
+            logger.error(f"Location {location.name} already exists in map!")
